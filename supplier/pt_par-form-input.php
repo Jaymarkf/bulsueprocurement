17f3cfcf714da9a2d578ac3d729a6bb0742de3cd
MAXX <fernandesjaymark@yahoo.com>
2020-11-05 21:21:28 +0800

    pt

--- supplier/pt_par-form-input.php
+++ supplier/pt_par-form-input.php
@@ -1,4 +1,8 @@
-
+<style>
+    .highlight{
+        background-color: #b3c2a1;
+    }
+</style>
 <div class="block">
     <div class="container-header text-center" style="position:relative;width:100%;height:40px;background-image:linear-gradient(#bababa, #454545); ">
         <span style="color:#e9e9e9;font-weight: bold;line-height: 35px;">Transfer PAR Data</span>
@@ -58,12 +62,13 @@ if(isset($_GET['pt'])){
     $id = $_GET['pt'];
     $cc = $conn->query('select * from tbl_par_items where par_id = '.$id);
 ?>
+<form>
 <div class="block">
     <div class="container-header text-center" style="position:relative;width:100%;height:40px;background-image:linear-gradient(#db9898, #d06868); ">
         <span style="color:#e9e9e9;font-weight: bold;line-height: 35px;">Selected Data</span>
     </div>
     <div class="container-fluid">
-        <table class="table" cellpadding="0" cellspacing="0">
+        <table class="table" cellpadding="0" cellspacing="0" id="tbl_id">
             <thead>
             <tr>
                 <th>Select</th>
@@ -79,9 +84,9 @@ if(isset($_GET['pt'])){
             while($rows = $cc->fetch_assoc()){
              ?>
                 <tr>
-                    <td><input type="radio" name="ck"/></td>
+                    <td><input type="radio" name="ck" class="ck" value="<?=$rows['id']?>"/></td>
                     <td><?=$rows['item_description']?></td>
-                    <td><input type="number" value="<?=$rows['quantity']?>" id="num"/></td>
+                    <td><input type="number" value="<?=$rows['quantity']?>" class="num" fixnum="<?=$rows['quantity']?>" readonly/></td>
                     <td><?=$rows['unit']?></td>
                     <td><?=$rows['unit_cost']?></td>
                     <td><?=$rows['total_cost']?></td>
@@ -93,6 +98,7 @@ if(isset($_GET['pt'])){
         </table>
     </div>
 </div>
+</form>
 <div class="block">
     <div class="container-fluid" style="padding:24px;">
         <div class="row">
@@ -123,6 +129,14 @@ if(isset($_GET['pt'])){
             <label style="display:block">Received By</label>
             <select class="form-control" name="received_by">
                 <option style="display:none" selected hidden disabled>select enduser</option>
+                <?php
+                    $sc = $conn->query("select * from tbl_supplier_employee");
+                    while($data_emp = $sc->fetch_assoc()){
+                        ?>
+                            <option value="<?=$data_emp['id']?>"><?=$data_emp['first_name'] . ' '. $data_emp['middle_name'] . ' '. $data_emp['last_name']?></option>
+                        <?php
+                    }
+                ?>
             </select>
         </div>
         <div class="span3">
@@ -144,14 +158,24 @@ if(isset($_GET['pt'])){
 ?>
 <script>
     $(document).ready(function(){
-        const cur_val = $('#num').val();
-       $('#num').change(function(){
+       $('.num').change(function(){
+           var cur_val = this.defaultValue;
             if(parseInt($(this).val()) > parseInt(cur_val)){
                 $(this).val(cur_val);
             }else if(parseInt($(this).val()) < 0 ){
                 $(this).val(0);
             }
        });
-        
+
+        $('#tbl_id tr').click(function () {
+            $(this).find('td input:radio').prop('checked', true);
+            $('.num').attr('readonly',true);
+            $(this).find('td input[type="number"]').attr('readonly',false);
+            $('#tbl_id tr').removeClass("highlight");
+            $(this).addClass("highlight");
+        });
+
+
+
     });
 </script>
