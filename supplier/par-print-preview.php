65491788fcfa338129c2165f9ec74a0aab431f9b
maxx <fernandesjaymark@yahoo.com>
2020-11-10 22:16:28 +0800

    fixing line in par and ics report

--- supplier/par-print-preview.php
+++ supplier/par-print-preview.php
@@ -69,13 +69,13 @@ $pdf->Text($pdf->GetX()+ 120,$pdf->GetY() - 40,'Received by: ');
 $pdf->Text($pdf->GetX()+10,$pdf->GetY() - 25,'Signature Over Printed Name of End User');
 $pdf->Line($pdf->GetX()+5,$pdf->GetY() - 28,$pdf->GetX()+100,$pdf->GetY()-28); //line 1
 $pdf->SetFont('Courier','B',9);
-$pdf->Text($pdf->GetX()+109,$pdf->GetY() - 25,'Signature Over Printed Name of Supply'); //line 2
+$pdf->Text($pdf->GetX()+109,$pdf->GetY() - 25,'Signature Over Printed Name of Supply');
 $pdf->Text($pdf->GetX()+120,$pdf->GetY() - 22.5,'and /or Property Custodian');
-$pdf->Line($pdf->GetX()+115,$pdf->GetY() - 28,$pdf->GetX()+180,$pdf->GetY()-28);
+$pdf->Line($pdf->GetX()+110,$pdf->GetY() - 28,$pdf->GetX()+180,$pdf->GetY()-28); 
 $pdf->SetFont('Courier','B',10);
 
 $pdf->Text($pdf->GetX()+127,$pdf->GetY() - 12,'Position / Office');
-$pdf->Line($pdf->GetX()+115,$pdf->GetY() - 15,$pdf->GetX()+180,$pdf->GetY()-15);
+$pdf->Line($pdf->GetX()+110,$pdf->GetY() - 15,$pdf->GetX()+180,$pdf->GetY()-15);
 
 
 $pdf->Text($pdf->GetX()+31,$pdf->GetY() - 12,'Position / Office');
@@ -85,7 +85,7 @@ $pdf->Text($pdf->GetX()+50,$pdf->GetY() - 2,'Date');
 $pdf->Line($pdf->GetX()+5,$pdf->GetY() - 5,$pdf->GetX()+100,$pdf->GetY()-5);
 
 $pdf->Text($pdf->GetX()+140,$pdf->GetY() - 2,'Date');
-$pdf->Line($pdf->GetX()+115,$pdf->GetY() - 5,$pdf->GetX()+180,$pdf->GetY()-5);
+$pdf->Line($pdf->GetX()+110,$pdf->GetY() - 5,$pdf->GetX()+180,$pdf->GetY()-5);
 
 
 
